deb408ceb24a6c91d0bae4b3b14635fe5b06d319
bennyziio <bapjoo2@gmail.com>
2018-12-12 20:01:58 +0900

    Updates

---
+++ precache-manifest.6ab60b63c4d034f7904883a91199f21a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "5876271a27047973fe38",
+    "url": "/movieql-client/static/js/runtime~main.5876271a.js"
+  },
+  {
+    "revision": "134ac4d6b6ddb6b5a92d",
+    "url": "/movieql-client/static/js/main.134ac4d6.chunk.js"
+  },
+  {
+    "revision": "8a43f7a43757944ce34c",
+    "url": "/movieql-client/static/js/1.8a43f7a4.chunk.js"
+  },
+  {
+    "revision": "f734040ce02be656ba4f55eeb40d341c",
+    "url": "/movieql-client/index.html"
+  }
+];
